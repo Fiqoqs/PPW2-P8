3469f8644a4d9d9c3b6596ec601585fdb8bb67ce
Syafiq Abdillah Habib <fiqabdillah8893@gmail.com>
2024-10-24 14:27:36 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\LoginRegisterController;
 use App\Http\Controllers\BukuController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/buku', [BukuController::class, 'index']);
@@ -9,3 +11,16 @@ Route::post('/buku', [BukuController::class, 'store' ])->name( 'buku.store');
 Route::delete('/buku/{id}', [BukuController::class, 'destroy' ])->name ( 'buku.destroy');
 Route::put('/buku/{id}', [BukuController::class, 'update' ])->name ( 'buku.update');
 Route::get('/buku/search', [BukuController::class, 'search'])->name('buku.search');
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::controller(LoginRegisterController::class)->group(function() {
+    Route::get('/register', 'register')->name('register');
+    Route::post('/store', 'store')->name('store');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/authenticate', 'authenticate')->name('authenticate');
+    Route::get('/dashboard', 'dashboard')->name('dashboard');
+    Route::post('/logout', 'logout')->name('logout');
+   });
+
+   
